c8d20259d81f0b8fc6b2d040babed54dbc9e736f
SheltonT200 <155662867+SheltonT200@users.noreply.github.com>
2024-08-14 14:47:02 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,38 +1,3 @@
-import { Sequence } from './src';
-
-interface Rss {
-  version: string;
-  channel: RssChannel;
-}
-
-interface RssChannel {
-  title: string;
-  description: string;
-  link: string;
-  copyright?: string;
-  lastBuildDate?: Date;
-  pubDate?: Date;
-  ttl?: number;
-  language?: string;
-  items: readonly RssItem[];
-}
-
-interface RssItem {
-  title: string;
-  description: string;
-  link: string;
-  comments?: readonly string[];
-  category?: string;
-  author?: string;
-  enclosure?: string;
-  guid?: string;
-  pubDate?: Date;
-  source?: string;
-}
-
-console.log(new Sequence([]));
-console.log(`Hello Node.js v${process.versions.node}!`);
-
 import { createServer } from 'http';
 
 const hostname = '127.0.0.1';
